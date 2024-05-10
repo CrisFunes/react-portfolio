10fb0b9bb977910f3600e535d63eef6f231cd9e1
Cris <funesdemian@gmail.com>
2024-05-10 19:41:49 -0300

    update component hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,53 @@
 import "./hero.scss"
+import {motion} from "framer-motion"
 
+const textVariants ={
+  initial:{
+    x: -500,
+    opacity: 0,
+  },
+  animate:{
+    x: 0,
+    opacity: 1,
+    trnsition:{
+      duration:1,
+      staggerChildren: 0.1,
+    }
+  },
+  scrollButton:{
+    opacity:0,
+    y: 10,
+    transition:{
+      duration:2,
+      repeat: Infinity,
+    },
+  },
+}
 const Hero = () => {
   return (
-    <div className="hero">Hero</div>
+    <div className="hero" >
+      <div className="wrapper">
+        <motion.div 
+        className="textContainer" 
+        variants={textVariants} 
+        initial="initial"
+        animate="animate" >
+          <motion.h2 variants={textVariants}>Cristian Funes</motion.h2>
+          <motion.h1 variants={textVariants}>Full-Stack Developer</motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants} >See the Latest Works</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img  variants={textVariants} animate="scrollButton" src="./scroll.png" alt="" />
+        </motion.div>
+      </div>
+      <div className="slidingTextContainer">
+        Python JavaScript MySQL AWS
+      </div>
+      <div className="imageContainer">
+        <img src="./hero.png" alt="" />
+      </div>
+    </div>
   )
 }
 
