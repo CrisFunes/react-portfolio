5b10011d7878cad7746af4b900f2eca24825b798
Cris <funesdemian@gmail.com>
2024-05-12 20:58:27 -0300

    create component parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Hero from "./components/hero/hero";
 import Navbar from "./components/navbar/navBar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return <div>
@@ -8,8 +9,8 @@ const App = () => {
       <Navbar/>  
       <Hero/>
     </section>
-    <section id="Services">Services</section>
-    <section id="Portfolio">Portfolio</section>
+    <section id="Services0"><Parallax type="services"/></section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section id="Contact">Contact</section>
     <section id="About">About</section>
   </div>;
