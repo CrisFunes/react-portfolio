8a3b42a2445d1a2e50cfc691b7c90e3a7a0f79c1
funesdemian <funesdemian@gmail.com>
2024-07-03 10:13:43 +0000

    Del parallax component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Parallax from "./components/parallax/Parallax";
 import Projects from "./components/projects/Projects";
 import Services from "./components/services/Services";
 
@@ -11,12 +10,6 @@ const App = () => {
     <section id="Homepage">
       <Navbar/>  
       <Hero/>
-    </section>
-    <section id="Services">
-      <Parallax type="services"/>
-    </section>
-    <section id="Portfolio">
-      <Parallax type="portfolio"/>
     </section>
     <section>
       <Services/>
