8ee2c725005d7dde7d3a80f93bf8cd1c36228a24
funesdemian <funesdemian@gmail.com>
2024-07-03 11:28:45 +0000

    add social icons animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,22 +2,41 @@ import Sidebar from '../sidebar/Sidebar'
 import './navbar.scss'
 import {motion} from "framer-motion"
 
+const textVariants = {
+  initial: {
+    x: 500,
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <Sidebar/>
+              <Sidebar/>
       <div className='wrapper'>
         <motion.span 
         initial={{opacity:0, scale:0.5}} 
         animate={{opacity:1, scale:1}} 
         transition={{duration:0.5}}
         ></motion.span>
-        <div className='social'>
+        <motion.div className='social'
+          variants={textVariants}
+          initial="initial"
+          animate="animate">
             <a href="https://github.com/CrisFunes"><img src="/github.png" alt="" /></a>
             <a href="mailto:funesdemian@gmail.com"><img src="/gmail.png" alt="" /></a>
             <a href="https://www.linkedin.com/in/cristian-funes-in/"><img src="/linkedin.png" alt="" /></a>
-        </div>
-
+        </motion.div>
       </div>
     </div>
   )
