cb2d2f76a7733b6be73e18d09add10f420ce9f3b
funesdemian <funesdemian@gmail.com>
2024-07-06 12:42:25 +0000

    Add image to service component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,116 +1,90 @@
-import { useRef } from 'react'
-import './services.scss'
-import {useInView, motion} from "framer-motion"
+import { useRef } from 'react';
+import './services.scss';
+import { useInView, motion } from 'framer-motion';
 
 const variant = {
-    initial:{
-        y:100,
-        opacity:0,
+  initial: {
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
     },
-    animate:{
-        x:0,
-        y:0,
-        opacity:1,
-        transition:{
-            duration:1,
-            straggerChildren:0.1,
-        },
-    }
-}
+  },
+};
 
 const Services = () => {
-    const ref = useRef()
+  const ref = useRef();
 
-    const isInView = useInView(ref, {margin:"-100px"})
-
+  const isInView = useInView(ref, { margin: '-100px' });
 
   return (
     <div className="about">
-        <motion.div 
-        className='services' 
-        variants={variant} 
-        initial="initial" 
+      <motion.div
+        className="services"
+        variants={variant}
+        initial="initial"
         ref={ref}
-        animate={isInView && "animate"}
-        >
-            <motion.div className='textContainer' variants={variant} >
-                <p>
-                    focus on helping your brand grow
-                    <br /> and move forward
-                </p>
-                <hr />
-            </motion.div>
-            <motion.div className='titleContainer'variants={variant} >
-                <div className="title">
-                    <img src='/people.webp' alt='' />
-                    <h1>
-                        <motion.b whileHover={{color: "orange" }}>Unique</motion.b> Ideas
-                    </h1>
-                </div>
-                <div className="title">
-                    <h1>
-                        <motion.b whileHover={{color: "orange" }}>For Your</motion.b> Business.
-                    </h1>
-                    <button>WHAT WE DO?</button>
-                </div>
-            </motion.div>
-            <motion.div className='listContainer' variants={variant} >
-                <motion.div className="box"
-                whileHover={{ background:"lightgray", color:"black"}}>
-                    <h2>Branding</h2>
-                    <p>In a galaxy far beyond the Milky Way, thirty worlds orbit a giant blue star.
-                        Each planet, unique in its own way, contributes to a cosmic symphony of 
-                        diversity and wonder. The largest, Zenthara, is a gas giant with swirling 
-                        storms of neon hues. Aridonia boasts vast deserts of crystal sands. 
-                        Lush with forests and waterfalls, Verdantia is a sanctuary of life. 
-                        Icelara, perpetually frozen, glimmers with ethereal ice structures. 
-                        
-                    </p>
-                    <button>Go</button>
-                </motion.div>
-                <motion.div className="box"
-                whileHover={{ background:"lightgray", color:"black"}}>
-                    <h2>Branding</h2>
-                    <p>In a galaxy far beyond the Milky Way, thirty worlds orbit a giant blue star.
-                        Each planet, unique in its own way, contributes to a cosmic symphony of 
-                        diversity and wonder. The largest, Zenthara, is a gas giant with swirling 
-                        storms of neon hues. Aridonia boasts vast deserts of crystal sands. 
-                        Lush with forests and waterfalls, Verdantia is a sanctuary of life. 
-                        Icelara, perpetually frozen, glimmers with ethereal ice structures. 
-                        
-                    </p>
-                    <button>Go</button>
-                </motion.div>
-                <motion.div className="box"
-                whileHover={{ background:"lightgray", color:"black"}}>
-                    <h2>Branding</h2>
-                    <p>In a galaxy far beyond the Milky Way, thirty worlds orbit a giant blue star.
-                        Each planet, unique in its own way, contributes to a cosmic symphony of 
-                        diversity and wonder. The largest, Zenthara, is a gas giant with swirling 
-                        storms of neon hues. Aridonia boasts vast deserts of crystal sands. 
-                        Lush with forests and waterfalls, Verdantia is a sanctuary of life. 
-                        Icelara, perpetually frozen, glimmers with ethereal ice structures. 
-                        
-                    </p>
-                    <button>Go</button>
-                </motion.div>
-                <motion.div className="box"
-                whileHover={{ background:"lightgray", color:"black"}}>
-                    <h2>Branding</h2>
-                    <p>In a galaxy far beyond the Milky Way, thirty worlds orbit a giant blue star.
-                        Each planet, unique in its own way, contributes to a cosmic symphony of 
-                        diversity and wonder. The largest, Zenthara, is a gas giant with swirling 
-                        storms of neon hues. Aridonia boasts vast deserts of crystal sands. 
-                        Lush with forests and waterfalls, Verdantia is a sanctuary of life. 
-                        Icelara, perpetually frozen, glimmers with ethereal ice structures. 
-                        
-                    </p>
-                    <button>Go</button>
-                </motion.div>
-            </motion.div>
+        animate={isInView && 'animate'}
+      >
+        <motion.div className="textContainer" variants={variant}>
+          <p>
+            focus on helping your brand grow
+            <br /> and move forward
+          </p>
+          <hr />
         </motion.div>
-    </div>
-  )
-}
 
-export default Services
+        <motion.div className="contentContainer" variants={variant}>
+          <div className="leftColumn">
+            <motion.div className="titleContainer" variants={variant}>
+              <div className="title">
+                <img src="/coffee.webp" alt="" />
+                <h1>
+                  <motion.b whileHover={{ color: 'orange' }}>About</motion.b> Me
+                </h1>
+              </div>
+              <div className="title">
+                <h1>
+                  <motion.b whileHover={{ color: 'orange' }}>My</motion.b> History
+                </h1>
+                <button>WHAT I DO?</button>
+              </div>
+            </motion.div>
+            <motion.div
+              className="imageContainer"
+              variants={variant}
+              initial="initial"
+              animate="animate"
+            >
+              <img src="/me.webp" alt="" />
+            </motion.div>
+          </div>
+          <motion.div className="listContainer" variants={variant}>
+            <motion.div className="box" whileHover={{ background: 'lightgray', color: 'black' }}>
+              <h2>Academic</h2>
+              <p>
+              "I am a software developer with over 2 years of experience
+               at Vistaguay Tech, leading complex and multidisciplinary 
+               projects in backend and frontend development with Laravel 
+               and React, data processing with Python, and scalable 
+               applications in AWS. I have effective communication and 
+               project delivery skills, as well as certifications in AWS and 
+               Laravel. I stay up-to-date on the latest trends in software 
+               development."
+              </p>
+              <button>Go</button>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Services;
