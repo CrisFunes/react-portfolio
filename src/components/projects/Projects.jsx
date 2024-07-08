d42e59072a5dde23442661afc4fba8946c1ebaa9
funesdemian <funesdemian@gmail.com>
2024-07-08 17:46:03 +0000

    style changes in projects component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -60,7 +60,6 @@ const Single = ({item}) => {
 
     const {scrollYProgress} = useScroll({
         target: ref, 
-        // offset: ["start start", "end start"],
     });
 
     const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
